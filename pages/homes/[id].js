dea1c0f9fdbbe245706e0160e14ac699bcfb2315
hadi-rahimii <0hadi.rahimi@gmail.com>
2024-02-06 15:33:50 +0330

    create folders

---
+++ pages/homes/[id].js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SingleDetailesPage() {
+  return <h1>SingleDetailesPage</h1>;
+}
---
+++ pages/homes/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function homes() {
+  return (
+    <div>all homes</div>
+  )
+}
