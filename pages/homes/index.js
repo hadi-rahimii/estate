a4805b52466b7e3467f9a468ce378c43c104d4e0
hadi-rahimii <0hadi.rahimi@gmail.com>
2024-02-06 23:41:22 +0330

    create homes index

--- pages/homes/index.js
+++ pages/homes/index.js
@@ -1,5 +1,50 @@
-import React from 'react'
+import React from "react";
+import db from "./../../data/db.json";
+import Home from "@/components/modules/Home";
 
-export default function homes() {
-  return <h1>all homes</h1>
+function index() {
+  return (
+    <div class="home-section" id="houses">
+      <div class="home-filter-search">
+        <div class="home-filter">
+          <select name="" id="">
+            <option value="" selected>
+              انتخاب کنید
+            </option>
+            <option value="">بر اساس قیمت</option>
+            <option value="">بر اساس تعداد اتاق</option>
+            <option value="">بر اساس ادرس</option>
+            <option value="">بر اساس اندازه</option>
+          </select>
+        </div>
+        <div class="home-search">
+          <input type="text" placeholder="جستجو کنید" />
+        </div>
+      </div>
+
+      <div class="homes">
+        {db.homes.slice(0, 6).map((home) => (
+          <Home key={home.id} {...home} />
+        ))}
+      </div>
+
+      <ul class="pagination__list">
+        <li class="pagination__item">
+          <a href="#" class=""></a>
+        </li>
+        <li class="pagination__item">
+          <a href="#" class="">
+            2
+          </a>
+        </li>
+        <li class="pagination__item active">
+          <a href="#" class="">
+            1
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
+
+export default index;
